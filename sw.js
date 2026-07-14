e4edf8df022999e0a6c283c7cfcc7bd057fc9761
lincecum2-alt <lincecum2@gmail.com>
2026-07-15 01:53:22 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE='project-y-v1';
-const ASSETS=['./','index.html','manifest.json','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='project-y-v11';const ASSETS=['./','index.html','manifest.json','icon-192.png','icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
